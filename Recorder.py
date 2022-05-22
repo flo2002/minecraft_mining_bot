3010e319e6ce44ae07a7b0296f773fb305b6da4b
flo2002 <florian.schiemer003@gmail.com>
2022-05-22 12:15:42 +0200

    fixed GUI

--- Recorder.py
+++ Recorder.py
@@ -1,9 +1,9 @@
 import threading
 
-
 class Recorder:
     def __init__(self):
         threading.Thread.__init__(self)
+        
         
     def record(self):
         print("Recording...")
--- main.py
+++ main.py
@@ -2,32 +2,37 @@
 from Replayer import Replayer
 from Recorder import Recorder
 
-from tkinter import Tk, Label, Button, StringVar
+from tkinter import Tk, Label, Button, StringVar, Frame
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("A simple Minecraft mining bot")
 
-        self.label_index = 0
         self.label_text = StringVar()
         self.label_text.set("Choose an option: ")
         self.label = Label(master, textvariable=self.label_text)
         self.label.pack()
 
+        self.button_frame = Frame(master)
+        self.button_frame.pack()
+        
         self.record_button = Button(master, text="Record", command=self.record)
-        self.record_button.pack()
-
+        self.record_button.pack(in_=self.button_frame, side="left")
         self.replay_button = Button(master, text="Replay", command=self.replay)
-        self.replay_button.pack()
+        self.replay_button.pack(in_=self.button_frame, side="right")
+        
+        self.status_label_text = StringVar()
+        self.status_label = Label(master, textvariable=self.status_label_text)
+        self.status_label.pack()
 
     def record(self):
-        recorder = Recorder()
-        recorder.record()
+        self.status_label_text.set("Start the recording with \"g\" and stop it with \"h\"")
+        Recorder().record()
 
     def replay(self):
-        replayer = Replayer()
-        replayer.replay()
+        self.status_label_text.set("Start the replaying with \"g\" and stop it with \"h\"")
+        Replayer().replay()
 
 root = Tk()
 root.geometry("400x100")
